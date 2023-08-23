3b4d1c09d16346f026702aa3fdfef90ed96483cd
Chivier Humber <chivier.humber@outlook.com>
2023-08-23 20:45:31 +0800

    Compare with direct mode

--- sgg/sgg.cu
+++ sgg/sgg.cu
@@ -5,6 +5,56 @@
 const int kWarmUpTurns = 100;
 const size_t kWarmUpSize = 16 * 1024 * 1024; // 16 MB
 
+float DirectCopyTest(int device_id1, int device_id2, size_t size) {
+    int *pointers[2];
+
+    cudaSetDevice(device_id1);
+    // cudaDeviceEnablePeerAccess(device_id2, 0);
+    cudaMalloc(&pointers[0], size);
+
+    cudaSetDevice(device_id2);
+    // cudaDeviceEnablePeerAccess(device_id1, 0);
+    cudaMalloc(&pointers[1], size);
+
+    cudaEvent_t begin, end;
+    cudaEventCreate(&begin);
+    cudaEventCreate(&end);
+
+    // Check available
+    // int avail;
+    // cudaDeviceCanAccessPeer(&avail, device_id1, device_id2);
+    // printf("[%d]", avail);
+
+    // Warm Up
+    int index;
+    for (index = 0; index < kWarmUpTurns; ++index) {
+        cudaMemcpy(pointers[0], pointers[1], kWarmUpSize, cudaMemcpyDeviceToDevice);
+    }
+
+    cudaEventRecord(begin);
+    cudaMemcpy(pointers[0], pointers[1], size, cudaMemcpyDeviceToDevice);
+    cudaEventRecord(end);
+    cudaEventSynchronize(end);
+
+    float elapsed;
+    cudaEventElapsedTime(&elapsed, begin, end);
+    elapsed /= 1000;
+
+    cudaSetDevice(device_id1);
+    // cudaDeviceDisablePeerAccess(device_id2);
+    cudaFree(pointers[0]);
+
+    cudaSetDevice(device_id2);
+    // cudaDeviceDisablePeerAccess(device_id1);
+    cudaFree(pointers[1]);
+
+    cudaEventDestroy(end);
+    cudaEventDestroy(begin);
+
+    return elapsed;
+}
+
+
 float P2PCopyTest(int device_id1, int device_id2, size_t size) {
     int *pointers[2];
 
@@ -21,9 +71,9 @@ float P2PCopyTest(int device_id1, int device_id2, size_t size) {
     cudaEventCreate(&end);
 
     // Check available
-    int avail;
-    cudaDeviceCanAccessPeer(&avail, device_id1, device_id2);
-    printf("[%d]", avail);
+    // int avail;
+    // cudaDeviceCanAccessPeer(&avail, device_id1, device_id2);
+    // printf("[%d]", avail);
 
     // Warm Up
     int index;
@@ -61,7 +111,7 @@ int main() {
     if (numGPUs < 2) {
         std::cout << "Error: At least two GPUs are required." << std::endl;
         return 0;
-    }
+    
 
     printf("Bandwidth test result:\n");
 
@@ -69,6 +119,18 @@ int main() {
         for (int index2 = 0; index2 < numGPUs; index2++) {
             size_t data_size = 128 * 1024 * 1024 * sizeof(int);
             float time = P2PCopyTest(index1, index2, data_size);
+            float bandwidth = (data_size / 1024 / 1024 / 1024.0) / (time); // GB/s
+            printf("%10.2f,", bandwidth * bandwidth);
+        }
+        printf("\n");
+    }
+
+    printf("Bandwidth test result:\n");
+
+    for (int index1 = 0; index1 < numGPUs; index1++) {
+        for (int index2 = 0; index2 < numGPUs; index2++) {
+            size_t data_size = 128 * 1024 * 1024 * sizeof(int);
+            float time = DirectCopyTest(index1, index2, data_size);
             float bandwidth = (data_size / 1024 / 1024 / 1024.0) / (time); // GB/s
             printf("%10.2f,", bandwidth * bandwidth);
         }
